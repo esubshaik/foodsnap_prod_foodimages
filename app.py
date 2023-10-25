2f811ff8801b2402fcde7d93190dc8242878c04a
Shaik Esub <108605272+EsubShaik@users.noreply.github.com>
2023-10-25 23:42:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import os
 import base64
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for your app
 
 # Define a route to handle image requests
 @app.route('/get_images', methods=['GET'])
